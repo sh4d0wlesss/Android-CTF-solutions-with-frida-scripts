412f9f8f257130b47dbe663aebea558d4d955e86
sh4d0wless <34010238+sh4d0wlesss@users.noreply.github.com>
2023-10-13 16:25:28 +0300

    Update broadcast.js

--- juicybar/broadcast.js
+++ juicybar/broadcast.js
@@ -1,11 +1,23 @@
-//unintended solution for broadcast section flag-2
 Java.perform(function(){
+
     let BroadcastReceivers = Java.use("xyz.barsk.juicy.levels.tools.BroadcastReceivers");
     BroadcastReceivers["g"].implementation = function (intent) {
+
         console.log(`BroadcastReceivers.g is called: intent=${intent}`);
         let result = this["g"](intent);
-        console.log(`BroadcastReceivers.g result=${result}`);
-        console.log("Flag: JUICY{" + result + "}");
+        console.log(`Original flag result= JUICY{${result}}`);
+
+        //for getting flag 3, create an fake intent with expected action :)
+        const intentClass = Java.use("android.content.Intent");
+        var intentTT = intentClass.$new();
+        intentTT.setAction("xyz.barsk.juicy.action.KAMEN");
+        let result3 = this["g"](intentTT);
+        console.log(`Level 3 flag result=JUICY{${result3}}`);
+        //-----
+        
         return result;
     };
 });
+
+
+
